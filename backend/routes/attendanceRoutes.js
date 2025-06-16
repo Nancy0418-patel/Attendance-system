f41b04966efa35d6541406457a5c422844349a24
Nancy0418-patel <nanpdu800@gmail.com>
2025-06-16 16:07:56 +0530

    Delete backend/routes directory

--- backend/routes/attendanceRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/attendanceRoutes.js
-const express = require("express");
-const router = express.Router();
-const { markAttendance, getAttendanceByStudent } = require("../controllers/attendanceController");
-
-router.post("/", markAttendance);                        // POST /api/attendance
-router.get("/:studentId", getAttendanceByStudent);       // GET /api/attendance/:studentId
-
-module.exports = router;
