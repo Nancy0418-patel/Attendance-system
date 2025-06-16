527419f2879c7c9f373d47613482995eb77338ef
Nancy0418-patel <nanpdu800@gmail.com>
2025-06-16 16:18:21 +0530

    Add files via upload

---
+++ frontend/src/OneSignalInitializer.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const OneSignalInitializer = () => {
+  useEffect(() => {
+    console.log("OneSignal functionality has been disabled.");
+  }, []);
+
+  return null;
+};
+
+export default OneSignalInitializer; 
