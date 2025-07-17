775d385b3d67c6b9377ff5e00d4561978dcd46cb
Nancy0418-patel <nanpdu800@gmail.com>
2025-07-17 16:57:24 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,5 @@
 // Central API base URL for all frontend API calls
-const API_BASE_URL = "http://localhost:5000" ;
-// || "https://attendance-system-backend-fyqk.onrender.com" ;
+const API_BASE_URL = "http://localhost:5000" || "https://attendance-system-backend-fyqk.onrender.com" ;
 export default API_BASE_URL;
 // const API_BASE_URL =
 //   window.location.hostname === "localhost"
